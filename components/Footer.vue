24905aa2dd0d074705aa9c637299a6c2d3e899ca
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-01-14 16:09:07 +0600

    On Devlopment

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <footer class="bg-gray-100 text-center py-4 mt-8">
-    <p class="text-gray-600">© 2025 Nuxt Store. All rights reserved.</p>
+    <p class="text-gray-600">© 2025 Nuxt Mart. All rights reserved.</p>
     <div class="flex justify-center space-x-4">
             <div v-for="item in footItems" :key="item.name">
               <NuxtLink :to="item.link" class="nav-link">
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto p-6">
     <div class="flex flex-col md:flex-row items-center bg-blue-50 p-10">
       <div class="text-section md:w-1/2">
-        <h1 class="text-6xl font-bold mb-4 text-green-600">Nuxt Store</h1>
+        <h1 class="text-6xl font-bold mb-4 text-green-600">Nuxt Mart</h1>
         <h3 class="text-3xl mb-7 leading-tight">
           Shop Smarter<br />
           Not Harder!
